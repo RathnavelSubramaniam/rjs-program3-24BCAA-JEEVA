1233f3570c8a751bd4948102aa220514124adb25
24BCAA-JEEVA <jeevabca666@gmail.com>
2026-07-03 11:36:49 +0530

    Update oddEven.js

--- oddEven.js
+++ oddEven.js
@@ -1,6 +1,11 @@
 // Write an arrow function to check whether a number is Odd or Even.
 // Return "Even" if the number is even.
 // Return "Odd" if the number is odd.
+
+// Example:
+// checkOddEven(8) ? "Even"
+// checkOddEven(5) ? "Odd"
+
 const checkOddEven = (num) => {
     if (num % 2 === 0) {
         return "Even";
@@ -9,5 +14,7 @@ const checkOddEven = (num) => {
     }
 };
 
-let number = 12;
+let number = 11;
 console.log(number + " is " + checkOddEven(number) + " Number");
+
+module.exports = checkOddEven;
